39e7b8180871402d6a7ee0f3416375bf8ad988c8
ilyav <ilyav@GOLDY>
2025-09-05 21:59:22 +0300

    Operator+ DONE

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include<windows.h>
+#include<time.h>
 using namespace std;
 using std::cout;
 using std::cin;
 using std::endl;
 
 #define tab "\t"
-#define string cout<<"---------------------------\n"<<endl
-//#define BASE_CHECK
+#define string "\n----------------------------------------------------\n"
+
 
 class Element
 {
@@ -18,38 +19,70 @@ public:
 	{
 		this->Data = Data;
 		this->pNext = pNext;
-		cout << "EConstructor:\t" << this << endl;
+		//cout << "EConstructor:\t" << this << endl;
 	}
 	~Element()
 	{
-		cout << "EDestructor:\t" << this << endl;
+		//cout << "EDestructor:\t" << this << endl;
 	}
 	friend class ForwardList;
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
 };
 
 class ForwardList
 {
 	Element* Head;
-	int size_list;
+	size_t size;
+
 public:
+	Element* get_Head()const
+	{
+		return Head;
+	}
+	size_t get_size()const
+	{
+		return size;
+	}
 	ForwardList()
 	{
 		Head = nullptr;
-		size_list = 0;
+		size = 0;
 		cout << "FLConstructor:\t" << this << endl;
+	}
+	ForwardList(const ForwardList& other):ForwardList()
+	{
+		*this = other;
+		cout << "FLCopyConstructor:\t" << this << endl;
 	}
 	~ForwardList()
 	{
-		cout << "FLDestructor:\t" << this << endl;
+		clock_t start = clock();
+		while (Head)pop_front();
+		clock_t end = clock();
+		cout << "FLDestructor:\t" << this <<"\tcomplete for: " <<double(end-start)/CLOCKS_PER_SEC << endl;
+	}
+	
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+		{
+			push_back(Temp->Data);
+		}
+		cout << "FLCopyAssignment:\t" << this << endl;
+		return *this;
 	}
 
 	void push_front(int Data)
 	{
-		Element* New = new Element(Data);
+		Head = new Element(Data, Head);
+		/*Element* New = new Element(Data);
 
 		New->pNext = Head;
-		Head = New;
-		size_list++;
+		Head = New;*/
+		size++;
 	}
 	
 	void push_back(int Data)
@@ -64,12 +97,12 @@ public:
 			Temp = Temp->pNext;
 		}
 		Temp->pNext = new Element(Data);
-		size_list++;
+		size++;
 
 	}
 	void insert(int Data, int index)
 	{
-		if (index > size_list)
+		if (index > size)
 		{
 			push_back(Data);
 			cout << "Индекс выходит за пределы списка - поставили в конец" << endl;
@@ -86,23 +119,24 @@ public:
 		{
 			Previous = Previous->pNext;
 		}
-		Element* New = new Element(Data);
+		Previous->pNext = new Element(Data, Previous->pNext);
+		/*Element* New = new Element(Data);
 		New->pNext = Previous->pNext;
-		Previous->pNext = New;
-		size_list++;
+		Previous->pNext = New;*/
+		size++;
 	}
 	void pop_front()
 	{
+		
 		while (Head == nullptr)break;
 		Element* to_delete = Head;
 		Head = Head->pNext;
 		delete to_delete;
-		size_list--;
+		size--;
 	}
 	void pop_back()
 	{
-		while (Head == nullptr)break;
-		if (Head->pNext == nullptr)
+		if (!Head||Head->pNext == nullptr)
 		{
 			pop_front();
 		}
@@ -113,11 +147,11 @@ public:
 		}
 		delete Temp->pNext;
 		Temp->pNext = nullptr;
-		size_list--;
+		size--;
 	}
 	void erase(int index)
 	{
-		if (index >= size_list)
+		if (index >= size)
 		{
 			cout << "Ошибка: Индекс выходит за пределы списка!" << endl;
 		}
@@ -137,19 +171,41 @@ public:
 		Previous->pNext = to_delete->pNext;
 		
 		delete to_delete;
-		size_list--;
+		size--;
 	}
 
 	void print()const
 	{
-		Element* Temp = Head;
+		/*Element* Temp = Head;
 		while (Temp)
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp=Temp->pNext;
+		}*/
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 		}
 	}
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
 };
+ForwardList operator+(const ForwardList& left, const ForwardList& right)
+{
+	ForwardList fusion;
+	for (Element* Temp = left.get_Head(); Temp; Temp = Temp->pNext)
+	{
+		fusion.push_back(Temp->Data);
+	}
+	for (Element* Temp = right.Head; Temp; Temp = Temp->pNext)
+	{
+		fusion.push_back(Temp->Data);
+	}
+	return fusion;
+}
+
+//#define BASE_CHECK
+#define OPERATOR_PLUS_CHECK
+//#define PERFORMANCE_CHECK
 
 void main()
 {
@@ -190,6 +246,7 @@ void main()
 	list.print();
 #endif
 
+#ifdef OPERATOR_PLUS_CHECK
 	ForwardList list1;
 	list1.push_back(0);
 	list1.push_back(1);
@@ -208,8 +265,36 @@ void main()
 	list2.push_back(89);
 	list2.print();
 
-	int index, value;
+	cout << string << endl;
+	ForwardList fusion;
+	cout << string << endl;
+	fusion = list1 + list2;
+	cout << string << endl;
+
+	fusion.print();
+
+	/*int index, value;
 	cout << "Go to value & index: "; cin >> value >> index;
 	list1.insert(value, index);
-	list1.print();
+	list1.print();*/
+
+#endif
+	
+#ifdef PERFORMANCE_CHECK
+	int n;
+	cout << "Go to size list: "; cin >> n;
+	ForwardList list;
+	clock_t start = clock();
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	clock_t end = clock();
+	cout << "ForwardList filled for "<<double(end-start)/CLOCKS_PER_SEC<<" second "<<endl;
+	system("PAUSE");
+
+#endif 
+
+
+
 }
