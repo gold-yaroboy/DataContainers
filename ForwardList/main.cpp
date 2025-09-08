6a023f56f83fe7ae1b30632b9b4f1189c29af076
ilyav <ilyav@GOLDY>
2025-09-08 21:58:52 +0300

    Performance DONE

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -54,6 +54,17 @@ public:
 		*this = other;
 		cout << "FLCopyConstructor:\t" << this << endl;
 	}
+	ForwardList(int size) :ForwardList()
+	{
+		while (size--)push_front(0);
+		cout << "FLSizeConstructor:\t" << this << endl;
+	}
+
+	ForwardList(ForwardList&& other) :ForwardList()
+	{
+		*this = std::move(other);
+	}
+
 	~ForwardList()
 	{
 		clock_t start = clock();
@@ -69,12 +80,39 @@ public:
 
 		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
 		{
-			push_back(Temp->Data);
+			push_front(Temp->Data);
 		}
+		reverse();
 		cout << "FLCopyAssignment:\t" << this << endl;
 		return *this;
 	}
 
+	ForwardList& operator=(ForwardList&& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+		this->Head = other.Head;
+		this->size = other.size;
+		other.Head = nullptr;
+		other.size = 0;
+		cout << "FLMoveAssognment:\t" << this << endl;
+		return *this;
+	}
+
+	int operator[](int index)const
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		return Temp->Data;
+	}
+	int& operator[](int index)
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		return Temp->Data;
+	}
+
+	
 	void push_front(int Data)
 	{
 		Head = new Element(Data, Head);
@@ -89,7 +127,7 @@ public:
 	{
 		if (Head == nullptr)
 		{
-			push_front(Data);
+			return push_front(Data);
 		}
 		Element* Temp = Head;
 		while (Temp->pNext != nullptr)
@@ -149,6 +187,8 @@ public:
 		Temp->pNext = nullptr;
 		size--;
 	}
+
+
 	void erase(int index)
 	{
 		if (index >= size)
@@ -174,6 +214,20 @@ public:
 		size--;
 	}
 
+	void reverse()
+	{
+		ForwardList reverse;				   //будет хранить задом на перед
+		while (Head)				       //пока список содержит элементы
+		{
+			reverse.push_front(Head->Data); //добавляем голову элемент
+			pop_front();				   //и удаляем начальный элемент списка
+		}
+		*this = std::move(reverse);
+		/*Head = reverse.Head;*/				   //подменяем наш список Реверсным
+		/*size = reverse.size;*/
+		reverse.Head = nullptr;			   //поскольку Реверсный список является локальной переменной для него будет вызван деструктор
+	}									   //который полностью его очистит, а он указывает на ту же паямть что и наш основной список
+
 	void print()const
 	{
 		/*Element* Temp = Head;
@@ -194,18 +248,22 @@ ForwardList operator+(const ForwardList& left, const ForwardList& right)
 	ForwardList fusion;
 	for (Element* Temp = left.get_Head(); Temp; Temp = Temp->pNext)
 	{
-		fusion.push_back(Temp->Data);
+		fusion.push_front(Temp->Data);
 	}
 	for (Element* Temp = right.Head; Temp; Temp = Temp->pNext)
 	{
-		fusion.push_back(Temp->Data);
+		fusion.push_front(Temp->Data);
 	}
+	fusion.reverse();
 	return fusion;
 }
 
 //#define BASE_CHECK
-#define OPERATOR_PLUS_CHECK
+//#define OPERATOR_PLUS_CHECK
 //#define PERFORMANCE_CHECK
+//#define SUBSCRIPT_OPERATOR_CHECK
+//#define COPY_SEMANTIC_PERFORMANS_CHECK
+#define MOVE_SEMANTIC_CHECK
 
 void main()
 {
@@ -252,8 +310,13 @@ void main()
 	list1.push_back(1);
 	list1.push_back(1);
 	list1.push_back(2);
-	list1.print(); 
-	
+	list1.print();
+	for (int i = 0; i < list1.get_size(); i++)
+	{
+		cout << list1[i] << tab;
+	}
+	cout << endl;
+
 	ForwardList list2;
 	list2.push_back(3);
 	list2.push_back(5);
@@ -264,14 +327,25 @@ void main()
 	list2.push_back(55);
 	list2.push_back(89);
 	list2.print();
+	for (int i = 0; i < list2.get_size(); i++)
+	{
+		cout << list2[i] << tab;
+	}
+	cout << endl;
 
 	cout << string << endl;
-	ForwardList fusion;
+	ForwardList list3;
 	cout << string << endl;
-	fusion = list1 + list2;
+	list3 = list1 + list2;
 	cout << string << endl;
 
-	fusion.print();
+	//list3.print();
+
+	for (int i = 0; i < list3.get_size(); i++)
+	{
+		cout << list3[i] << tab;
+	}
+	cout << endl;
 
 	/*int index, value;
 	cout << "Go to value & index: "; cin >> value >> index;
@@ -279,7 +353,7 @@ void main()
 	list1.print();*/
 
 #endif
-	
+
 #ifdef PERFORMANCE_CHECK
 	int n;
 	cout << "Go to size list: "; cin >> n;
@@ -290,11 +364,80 @@ void main()
 		list.push_front(rand() % 100);
 	}
 	clock_t end = clock();
-	cout << "ForwardList filled for "<<double(end-start)/CLOCKS_PER_SEC<<" second "<<endl;
+	cout << "ForwardList filled for " << double(end - start) / CLOCKS_PER_SEC << " second " << endl;
 	system("PAUSE");
 
 #endif 
 
+#ifdef SUBSCRIPT_OPERATOR_CHECK
+	int n;
+	cout << "Go to size: "; cin >> n;
 
+	clock_t start;
+	clock_t end;
 
+	ForwardList list(n);
+	start = clock();
+	for (int i = 0; i < list.get_size(); i++)
+	{
+		list[i] = rand() % 100;
+	}
+	end = clock();
+	cout << "List filed " << double(end - start) / CLOCKS_PER_SEC << " second" << endl;
+	system("PAUSE");
+	for (int i = 0; i < list.get_size(); i++)
+	{
+		cout << list[i] << tab;
+	}
+	cout << endl;
+#endif
+
+#ifdef COPY_SEMANTIC_PERFORMANS_CHECK
+
+	int n;
+	cout << "Go to size: "; cin >> n;
+
+	clock_t start;
+	clock_t end;
+
+	ForwardList list1;
+
+	start = clock();
+	for (int i = 0; i < n; i++)
+	{
+		list1.push_front(rand() % 100);
+	}
+	end = clock();
+	cout << "List filed: " << double(end - start) / CLOCKS_PER_SEC << " second" << endl;
+	system("PAUSE");
+	start = clock();
+	ForwardList list2 = list1;
+	end = clock();
+	cout << "Copying complete for " << double(end - start) / CLOCKS_PER_SEC << " second" << endl;
+
+	for (int i = 0; i < list1.get_size(); i++)cout << list1[i] << tab; cout << endl;
+	for (int i = 0; i < list2.get_size(); i++)cout << list2[i] << tab; cout << endl;
+#endif
+
+#ifdef MOVE_SEMANTIC_CHECK
+
+	clock_t start, end;
+	ForwardList list1;
+	ForwardList list2;
+
+	start = clock();
+	for (int i = 0; i < 1000000; i++)list1.push_front(rand());
+	for (int i = 0; i < 1000000; i++)list2.push_front(rand());
+	end = clock();
+
+	cout << "Lists filed for " << double(end - start) / CLOCKS_PER_SEC << " seconds" << endl;
+	system("PAUSE");
+
+	start = clock();
+	ForwardList list3 = list1 + list2;
+	end = clock();
+
+	cout << "Lists concatenated for " << double(end - start) / CLOCKS_PER_SEC << " seconds" << endl;
+
+#endif
 }
